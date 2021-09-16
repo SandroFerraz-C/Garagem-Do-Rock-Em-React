6cec16170a6655500c0d1bf3029ed143d0785080
Sandro Ferraz <sergiosandro86@gmail.com>
2021-09-16 14:33:54 -0300

    I took the components from 'index.js' and put them in 'App.js'

--- src/index.js
+++ src/index.js
@@ -1,17 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import Header from './components/Header/Header';
-import History from './components/History/History';
-import Bands from './components/Bands/Bands';
-import './index.css';
+import App from './App';
+
 
 ReactDOM.render(
     <>
-    <Header />
-    <History />
-    <Bands />
+    <App />
     </>,
     document.getElementById('root')
-    
 )
